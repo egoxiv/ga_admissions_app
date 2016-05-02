62b335a98cece9596d6ced5754777883fc0ddb38
Erik Gomez <egoxiv@gmail.com>
2016-05-02 16:55:37 -0700

    Adds partials folder with jQuery and bootstrap CDN, completes simple mock up of Welcome page.

--- app.js
+++ app.js
@@ -5,29 +5,23 @@ var bodyParser = require('body-parser');
 var session    = require('express-session');
 var app        = express();
 var db         = require('./config/db');
-var cookieParser = require('cookie-parser');
-
-// auth
-var passport = require('passport');
-var GithubStrategy = require('passport-github').Strategy;
 
 // Routes
 var submitRoute = require('./routes/submit-routes/submit-routes');
 
 app.set('view engine', 'ejs');
-app.use( express.static(__dirname + '/public') );
+app.use( express.static(path.join(__dirname + '/public')));
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({ secret: 'unicorns', resave: false, saveUninitialized: false }));
 
-// passport stuff - http://blog.revathskumar.com/2014/06/express-github-authentication-with-passport.html
-app.use(cookieParser());
-app.use(passport.initialize());
-app.use(passport.session());
+app.get('/', function(req, res) {
+  res.render('welcome/welcome');
+});
 
-app.use('/submit', submitRoute);
+app.use('/submits', submitRoute);
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
--- public/submit/js/index.js
+++ public/submit/js/index.js
@@ -1,20 +1,5 @@
 $(function(){
 
-  $('#submit').on('click', function() {
-
-    $.ajax({
-      url: 'http://localhost:3000/sign-up',
-      method: 'POST',
-      success: function(data) {
-        console.log('SUCCESS');
-        console.log(data);
-      },
-      error: function(error) {
-        console.log('FAIL');
-        console.log(error);
-      }
-    });
-
-  });
+  console.log('Submit');
 
 });
