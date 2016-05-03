9571de82db0171d4ea42cad9805f465fd7a468cc
Matthew Parvinsmith <mrparvinsmith@gmail.com>
2016-05-02 17:19:55 -0700

    can post a new cohort, but with some glitches

--- controllers/cohorts.js
+++ controllers/cohorts.js
@@ -16,6 +16,30 @@ cohorts.new = function(req, res){
 
 cohorts.create = function(req, res){
   var cohort = new Cohort();
+  cohort.program = req.body.program;
+  cohort.city = req.body.city;
+  cohort.campus = req.body.campus;
+  //dates are shifting one day back
+  cohort.start = new Date(req.body.start);
+  cohort.end = new Date(req.body.end);
+  var biggest = 0;
+  Cohort.find({campus: req.body.campus, program: req.body.program}, function(err, sections){
+    sections.forEach(function(section){
+      if(section.number > biggest){
+        biggest = section.number;
+      }
+    });
+    console.log(biggest);
+    cohort.number = biggest + 1;
+    if(cohort.save()){
+      Cohort.find({}, function(err, classes){
+        if(err) return res.json(err);
+        things = classes;
+        // redirects not working in any configuration
+        res.render('cohorts/index');
+      });
+    }
+  });
 };
 
 cohorts.show = function(req, res){
